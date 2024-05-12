19eaa09ef21a7b41f5f110c1bcdcf3d74b3c6a14
Rachelle Canta <iamrachellecanta@gmail.com>
2024-05-13 03:16:53 +0800

    Added Menu Items

--- fe/rice-system-app/src/assets/assets.js
+++ fe/rice-system-app/src/assets/assets.js
@@ -1,12 +1,86 @@
-import basket_icon from './basket_icon.png'
+import basket_icon from './basket_icon.jpg'
 import logo from './logo.png'
+import header_img from './header_img.png'
 import search_icon from './search_icon.png'
+import menu_1 from '../assets/menu_1.jpg'
+import menu_2 from '../assets/menu_2.jpg'
+import menu_3 from '../assets/menu_3.jpg'
+import menu_4 from '../assets/menu_4.jpg'
+import menu_5 from '../assets/menu_5.jpg'
+import menu_6 from '../assets/menu_6.jpg'
+import menu_7 from '../assets/menu_7.jpg'
+import menu_8 from '../assets/menu_8.jpg'
+import menu_9 from '../assets/menu_9.jpg'
+import menu_10 from '../assets/menu_10.jpg'
+import menu_11 from '../assets/menu_11.jpg'
+import menu_12 from '../assets/menu_12.jpg'
+import menu_13 from '../assets/menu_13.jpg'
+import menu_14 from '../assets/menu_14.jpg'
+import menu_15 from '../assets/menu_15.jpg'
+
 
 
 export const assets = {
     logo,
     basket_icon,
     search_icon,
+    header_img
 }
 
-
+export const menu_list =[
+    {
+        menu_name: "Master Chef Premium Dinorado 25kg",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Master Chef Hasmin 25kg",
+        menu_image: menu_2
+    }, 
+    {
+        menu_name: "Joseph Maharlika 25kg",
+        menu_image: menu_3
+    },
+    {   menu_name: "Coco Pandan Premium Rice 25kg",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Golden Star Sinandomeng 25kg",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Crystal Pure Dinorado 25kg",
+        menu_image: menu_6
+    }, {
+        menu_name: "Sweet Jasmine Supreme Super Denorado 25kg",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Lipeno Sinandomeng 25kg",
+        menu_image: menu_8
+    },
+    {
+        menu_name: "Lipeno Pure Dinurado 25kg",
+        menu_image: menu_9
+    }, {
+        menu_name: "Lipeno C-4 Dinurado 25kg",
+        menu_image: menu_10
+    },
+    {
+        menu_name: "Joseph Maharlika Super Special 5kg",
+        menu_image: menu_11
+    },
+    {
+        menu_name: "Princess Zara Jasmine 5kg",
+        menu_image: menu_12
+    }, {
+        menu_name: "Lipeno Sinandomeng 5kg",
+        menu_image: menu_13
+    },
+    {
+        menu_name: "Lipeno Pure Dinurado 5kg",
+        menu_image: menu_14
+    }, {
+        menu_name: "Lipeno C-4 Dinurado 5kg",
+        menu_image: menu_15
+    }
+]
